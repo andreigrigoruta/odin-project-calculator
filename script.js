a3fbbcaa72bd5b97ae6cc5c677f0d6817c3c9be2
Andrei Grigoruta <androk20@gmail.com>
2023-02-07 16:13:42 +0200

    Add features: add, substract, multiply, divide

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let firstOperand = "";
+let secondOperand = "";
+let currentOperation = null;
+let shouldResetScreen = false;
 
 const numberBtns = document.querySelectorAll(".digits");
 const operatorBtns = document.querySelectorAll(".operators");
@@ -6,8 +9,11 @@ const equalsBtn = document.getElementById("equals");
 const allClearBtn = document.getElementById("allclear");
 const clearBtn = document.getElementById("clear");
 const decimalBtn = document.getElementById("decimal");
-const userInput = document.getElementById("user-input");
-const resultScreen = document.getElementById("result");
+const lastOpScreen = document.getElementById("user-input");
+const currentOpScreen = document.getElementById("result");
+
+allClearBtn.addEventListener("click", clear);
+clearBtn.addEventListener("click", deleteNumber);
 
 numberBtns.forEach((button) =>
   button.addEventListener("click", () => appendNumber(button.textContent))
@@ -18,12 +24,63 @@ operatorBtns.forEach((button) =>
 );
 
 function appendNumber(number) {
-  userInput.textContent += number;
+  if (
+    currentOpScreen.textContent === "0" ||
+    shouldResetScreen ||
+    currentOpScreen.textContent === "ZERO DIV ER"
+  ) {
+    resetScreen();
+  }
+  currentOpScreen.textContent += number;
 }
 
 function setOperation(operator) {
-  firstOperand = userInput.textContent;
-  userInput.textContent = `${firstOperand} ${operator} `;
+  if (currentOpScreen.textContent === "") return;
+  if (currentOperation !== null) {
+    if (evaluate() === "zero") {
+      lastOpScreen.textContent = "";
+      return;
+    }
+  }
+  firstOperand = currentOpScreen.textContent;
+  currentOperation = operator;
+  lastOpScreen.textContent = `${firstOperand} ${operator} `;
+  shouldResetScreen = true;
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return;
+  if (currentOperation === "÷" && currentOpScreen.textContent === "0") {
+    currentOpScreen.textContent = "ZERO DIV ER";
+    return "zero";
+  }
+  secondOperand = currentOpScreen.textContent;
+  currentOpScreen.textContent = operate(
+    currentOperation,
+    firstOperand,
+    secondOperand
+  );
+  lastOpScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand}`;
+  currentOperation = null;
+}
+
+function resetScreen() {
+  currentOpScreen.textContent = "";
+  shouldResetScreen = false;
+}
+
+function clear() {
+  currentOpScreen.textContent = "0";
+  lastOpScreen.textContent = "";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperation = null;
+}
+
+function deleteNumber() {
+  currentOpScreen.textContent = currentOpScreen.textContent
+    .toString()
+    .slice(0, -1);
 }
 
 function add(a, b) {
@@ -47,14 +104,16 @@ function remainder(a, b) {
 }
 
 function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
   switch (operator) {
     case "+":
       return add(a, b);
       break;
     case "-":
-      return subtract(a, b);
+      return substract(a, b);
       break;
-    case "*":
+    case "x":
       return multiply(a, b);
       break;
     case "/":
